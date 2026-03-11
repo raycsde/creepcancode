996472d33c597ddd5b3e3c348dc9a949c57e2647
ray <89139395+raycsde@users.noreply.github.com>
2026-03-11 18:40:00 +0800

    test4.5

---
+++ food_sell.py
@@ -0,0 +1,7 @@
+food_tuple = ('afood', 'bfood', 'cfood', 'dfood', 'efood')
+for fd_now in food_tuple:
+    print(fd_now)
+
+food_tuple = ('afood_new', 'bfood_new', 'cfood', 'dfood', 'efood')
+for fd_now1 in food_tuple:
+    print(fd_now1)
