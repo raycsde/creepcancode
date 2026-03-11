0c896fd074e4018de2e2ae3a26592b4c5f64da40
ray <89139395+raycsde@users.noreply.github.com>
2026-03-11 18:05:49 +0800

    test4.2

---
+++ for_pizza.py
@@ -0,0 +1,7 @@
+# use for in pizza
+for_pizza = ['apizza', 'bpizza', 'cpizza']
+for fpiz in for_pizza:
+    print(fpiz.title())
+    print(f'I like {fpiz.title()} pizza!\n')
+
+print('I really like pizza!')
---
+++ pet_lovely.py
@@ -0,0 +1,6 @@
+#ray Mar10 pet_lovely.py -- make 3 animals in pets then print something.
+pet_lovely = ['dog', 'cat', 'bird']
+for pet in pet_lovely:
+    print(f'A {pet} would make a great pet.')
+
+print('Any of these animals would make a great pet!')
